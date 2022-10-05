b425ef3d665ef65d78be50c4eff16b4f03772f5d
Marcus Vinicius <marcusviny63@gmail.com>
2022-10-04 21:38:02 -0300

    Recomeçando direito :D

---
+++ Para Resolver/BEE1042.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void ordena(int *x, int *y, int z){
+    int aux = *x;
+    *y = aux;
+    
+
+}
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+}
